567adb6f62317be6557c0167b17ae16c41aa73a4
Divyashree0303 <divyashreeburugupalli@gmail.com>
2024-03-04 16:02:03 +0530

    add products

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,23 +5,23 @@ import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import Footer from "./_components/footer/footer"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faXmark, faArrowRight, faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css';
 // Prevent fontawesome from adding its CSS since we did it manually above:
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false; /* eslint-disable import/first */
- import "./styles.css";
+import "./styles.css";
 
 
 export default function RootLayout({ children }) {
 
-    const[menuOpen,setMenuOpen] = useState(false);
-    const[productsOpen,setProductsOpen] = useState(false);
-    const[toolsOpen,setToolsOpen]=useState(false);
-    const[showHamburger,setShowHamburger]=useState(true);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [productsOpen, setProductsOpen] = useState(false);
+    const [toolsOpen, setToolsOpen] = useState(false);
+    const [showHamburger, setShowHamburger] = useState(true);
 
     useEffect(() => {
-    
+
 
         const handleWindowResize = () => {
             if (window.innerWidth <= 768) {
@@ -34,9 +34,9 @@ export default function RootLayout({ children }) {
         return () => {
             window.removeEventListener('resize', handleWindowResize);
         };
-    },[]);
+    }, []);
 
-    
+
 
     const openMenu = () => {
         setMenuOpen(true);
@@ -44,121 +44,131 @@ export default function RootLayout({ children }) {
 
     };
 
-    const closeMenu =() => {
+    const closeMenu = () => {
         setMenuOpen(false);
         setShowHamburger(true);
         setProductsOpen(false);
     }
 
-    const toggleProducts =() =>{
+    const toggleProducts = () => {
         setProductsOpen(!productsOpen);
     }
 
-return (
-    <html lang="en">
-      <body style={{ margin: 0,fontFamily:"sans-serif",position:"relative",minHeight:"100vh"}}>
+    return (
+        <html lang="en">
+            <body style={{ margin: 0, fontFamily: "sans-serif", position: "relative", minHeight: "100vh" }}>
 
-      
 
-      
-            <nav className="navbar">
-                <div className="navbarLeft">
-                    
+
+
+                <nav className="navbar">
+                    <div className="navbarLeft">
+
                         <img src="/FG_logo.jpeg" alt="Logo" />
-                    
-                    <div className='developer'><p>Developer</p></div>
-                </div>
-                <div className="navbarRight">
-                <div className="rightList productsButton" ><Link href="/">Home</Link></div>
-                    <div className="rightList productsButton" ><Link href="/introduction">APIs</Link></div>
-                    <div className=" rightList products">
-                        <button className="productsButton">Docs</button>
-                        <div className="productsDropdownContent">
-                            <Link href="/globalBanking/overview">Global Banking</Link>
-                            <Link href="#">Product 2</Link>
-                            <Link href="#">Product 3</Link>
-                        </div>
-                    </div>
-                    <div className=" rightList tools">
-                        <button className="toolsButton">Tools</button>
-                        <div className="toolsDropdownContent">
-                            <Link href="/apiTester" target="_blank">API Testing</Link>
-                        </div>
-                    </div>
-                    <div className="rightList productsButton" ><Link href="/">Support</Link></div>
-                    <button className="loginButton"><Link style={{textDecoration:"none",color:"white",fontWeight:"bold"}} href="https://globalbanking.fintractglobal.com/signin">Login</Link></button>
-                </div>
-                <div className="hamburger" >
-                    <div>{!showHamburger?<FontAwesomeIcon  className="hamburgerIconOpen" onClick={closeMenu} icon={faXmark} />:<FontAwesomeIcon onClick={openMenu} className="hamburgerIcon" icon={faBars} />}</div>
-                </div>
-            </nav>
 
-            <div className={menuOpen?`menu`:``}>
-            {menuOpen && (
+                        <div className='developer'><p>Developer</p></div>
+                    </div>
+                    <div className="navbarRight">
+                        <div className="rightList productsButton" ><Link href="/">Home</Link></div>
+                        <div className="rightList productsButton" ><Link href="/introduction">APIs</Link></div>
+                        <div className=" rightList products">
+                            <button className="productsButton">Docs</button>
+                            <div className="productsDropdownContent">
+                                <Link href="/globalBanking/overview">Global Banking</Link>
+                                <Link href="#">Fraudify</Link>
+                                <Link href="#">B2B Payment</Link>
+                                <Link href="#">Finalytics</Link>
+                                <Link href="#">Chatbot</Link>
+                                <Link href="#">Transaction Monitoring</Link>
+                            </div>
+                        </div>
+                        <div className=" rightList tools">
+                            <button className="toolsButton">Tools</button>
+                            <div className="toolsDropdownContent">
+                                <Link href="/apiTester" target="_blank">API Testing</Link>
+                            </div>
+                        </div>
+                        <div className="rightList productsButton" ><Link href="/">Support</Link></div>
+                        <button className="loginButton"><Link style={{ textDecoration: "none", color: "white", fontWeight: "bold" }} href="https://globalbanking.fintractglobal.com/signin">Login</Link></button>
+                    </div>
+                    <div className="hamburger" >
+                        <div>{!showHamburger ? <FontAwesomeIcon className="hamburgerIconOpen" onClick={closeMenu} icon={faXmark} /> : <FontAwesomeIcon onClick={openMenu} className="hamburgerIcon" icon={faBars} />}</div>
+                    </div>
+                </nav>
+
+                <div className={menuOpen ? `menu` : ``}>
+                    {menuOpen && (
                         <div>
                             <ul>
                                 <li><Link href="/introduction">APIs</Link></li>
-                                <li><div onClick={()=> {
+                                <li><div onClick={() => {
                                     setProductsOpen(true);
                                     setMenuOpen(false);
                                 }} >Docs <FontAwesomeIcon icon={faArrowRight} /></div></li>
-                                <li><div onClick={()=> {
+                                <li><div onClick={() => {
                                     setToolsOpen(true);
                                     setMenuOpen(false);
                                 }} >Tools <FontAwesomeIcon icon={faArrowRight} /></div></li>
                                 <li><div><Link href="/">Support</Link></div></li>
                             </ul>
 
-                            <button className="loginButton"><Link style={{textDecoration:"none",color:"white"}} href="https://globalbanking.fintractglobal.com/signin">Login</Link></button>
+                            <button className="loginButton"><Link style={{ textDecoration: "none", color: "white" }} href="https://globalbanking.fintractglobal.com/signin">Login</Link></button>
                         </div>
-                        )}
-                    
-                    {productsOpen &&(
+                    )}
+
+                    {productsOpen && (
                         <div className='menu'>
-                        
-                        <div className='menuTitle'>
-                        <FontAwesomeIcon icon={faArrowLeft} onClick={() => {setProductsOpen(false);
-                        setMenuOpen(true);}}/>
-                        <div>Docs</div>
+
+                            <div className='menuTitle'>
+                                <FontAwesomeIcon icon={faArrowLeft} onClick={() => {
+                                    setProductsOpen(false);
+                                    setMenuOpen(true);
+                                }} />
+                                <div>Docs</div>
+
+                            </div>
+                            <ul>
+                                <li><Link href="/globalBanking">Global Banking</Link></li>
+                                <li><Link href="#">Fraudify</Link></li>
+                                <li><Link href="#">B2B Payment</Link></li>
+                                <li><Link href="#">Finalytics</Link></li>
+                                <li><Link href="#">Chatbot</Link></li>
+                                <li><Link href="#">Transaction Monitoring</Link></li>
+                            </ul>
 
                         </div>
-                        <ul>
-                        <li><Link href="/globalBanking">Global Banking</Link></li>
-                        <li><Link href="#">Product 2</Link></li>
-                        <li><Link href="#">Product 3</Link></li>
-                    </ul>
-                    
-                    </div>
-            )}
+                    )}
 
-            {toolsOpen &&(
+                    {toolsOpen && (
                         <div className='menu'>
-                        
-                        <div className='menuTitle'>
-                        <FontAwesomeIcon icon={faArrowLeft} onClick={() => {setToolsOpen(false);
-                        setMenuOpen(true);}}/>
-                        <div>Tools</div>
+
+                            <div className='menuTitle'>
+                                <FontAwesomeIcon icon={faArrowLeft} onClick={() => {
+                                    setToolsOpen(false);
+                                    setMenuOpen(true);
+                                }} />
+                                <div>Tools</div>
+
+                            </div>
+                            <ul>
+                                <li><Link href="/apiTester">API Testing</Link></li>
+
+                            </ul>
 
                         </div>
-                        <ul>
-                        <li><Link href="/apiTester">API Testing</Link></li>
-                        
-                    </ul>
-            
-                    </div>
-            )}
-                    
+                    )}
+
                 </div>
-             
-           
 
-        <SessionProvider  session={Session}>
-          {children}
-        </SessionProvider>
 
-       <Footer/>
-    
-      </body>
-    </html>)
 
-                        }
+                <SessionProvider session={Session}>
+                    {children}
+                </SessionProvider>
+
+                <Footer />
+
+            </body>
+        </html>)
+
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBuildingColumns,faCode } from '@fortawesome/free-solid-svg-icons'
+import { faBuildingColumns,faCode,faUserSecret,faCreditCard,faChartLine,faComment,faMagnifyingGlassDollar} from '@fortawesome/free-solid-svg-icons'
 import './styles.css'
 
 export default function Body() {
@@ -25,12 +25,44 @@ export default function Body() {
                         <FontAwesomeIcon className="card-icon" icon={faBuildingColumns} />
                         <div>
                             <Link href='/globalBanking/overview'>Global Banking</Link>
-                            <p>Global Banking Integration Solutions</p>
+                            <p>Global Banking integration solutions</p>
                         </div>
                     </div>
-                    <div className="card">Product 2</div>
-                    <div className="card">Product 3</div>
-                    <div className="card">Product 4</div>
+                    <div className="card">
+                        <FontAwesomeIcon className="card-icon" icon={faUserSecret} />
+                        <div>
+                            <Link href='#'>Fraudify</Link>
+                            <p>Real-time fraud detection & prevention</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <FontAwesomeIcon className="card-icon" icon={faCreditCard} />
+                        <div>
+                            <Link href='#'>B2B Payment</Link>
+                            <p>Platform for banking and finance</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <FontAwesomeIcon className="card-icon" icon={faChartLine} />
+                        <div>
+                            <Link href='#'>Finalytics</Link>
+                            <p>Unleashing AI powered market insights</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <FontAwesomeIcon className="card-icon" icon={faComment} />
+                        <div>
+                            <Link href='#'>Chatbot</Link>
+                            <p>Advanced chatbot for empowering conversations</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <FontAwesomeIcon className="card-icon" icon={faMagnifyingGlassDollar} />
+                        <div>
+                            <Link href='#'>Transaction Monitoring</Link>
+                            <p>UI powered anti-money laundering solution</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
